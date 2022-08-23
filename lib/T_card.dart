7ecf80f57fb7ffd274d1f2314361c4f3735c762c
juhojang <juhojang@hotmail.com>
2022-08-23 14:16:25 +0900

    버튼 디자인 변경

--- lib/T_card.dart
+++ lib/T_card.dart
@@ -96,116 +96,139 @@ class _TCardPageState extends State<TCardPage> {
     return Scaffold(
       body: Stack(
         children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 50, 0, 0),
+            child: Text("우산공유자 선택",style: TextStyle(fontSize: 40,color: Colors.lightBlue)),
+          ),
           !buttonTap?AnimatedOpacity(opacity: 0.1,duration: Duration(seconds: 1),child: Image(image: AssetImage('assets/images/rainy.gif'),fit: BoxFit.cover,height: double.infinity,))
               :AnimatedOpacity(opacity: 0,duration: Duration(seconds: 1),child: Image(image: AssetImage('assets/images/rainy.gif'),fit: BoxFit.cover,height: double.infinity,)),
-          !buttonTap?Center(
-          child: Column(
+          !buttonTap?Column(
             children: <Widget>[
-              SizedBox(height: 100),
-              TCard(
-                size: Size(400, 600),
-                cards: List.generate(
-                  widget.markers.length~/2,
-                      (int index) {
-                        double distance1=Distance(widget.locationX!, widget.locationY!,
-                            widget.markers[2*index+1].position.longitude,widget.markers[2*index+1].position.latitude);
-                        print(distance1);
-                        double distance2=Distance(widget.markers[0].position.longitude, widget.markers[0].position.latitude,
-                            widget.markers[2*index+2].position.longitude,widget.markers[2*index+2].position.latitude);
-                    return Container(
-                      color: Colors.lightBlue,
-                      alignment: Alignment.center,
-                      child: Column(
-                        children: [
-                          Spacer(),
-                          Text(
-                            '공유자 ${index + 1}',
-                            style: TextStyle(fontSize: 20.0, color: Colors.white),
-                          ),
-                          Spacer(),
-                          Container(
-                            width: 400,
-                            height: 400,
-                            child: GoogleMap(
-                              initialCameraPosition: _initialPosition,
-                              mapType: MapType.normal,
-                              myLocationEnabled: true,
-                              myLocationButtonEnabled: true,
-                              onMapCreated: (controller) {
-                                setState(() {
-                                  mapController = controller;
-                                });
-                              },
-                              onTap: (cordinate) {
-                                mapController.animateCamera(CameraUpdate.newLatLng(cordinate));
-                              },
-                              markers:{widget.markers[0],widget.markers[2*index+1],widget.markers[2*index+2]},
+              SizedBox(height: 120),
+
+              Center(
+                child: TCard(
+                  size: Size(400, 600),
+                  cards: List.generate(
+                    widget.markers.length~/2,
+                        (int index) {
+                          double distance1=Distance(widget.locationX!, widget.locationY!,
+                              widget.markers[2*index+1].position.longitude,widget.markers[2*index+1].position.latitude);
+                          print(distance1);
+                          double distance2=Distance(widget.markers[0].position.longitude, widget.markers[0].position.latitude,
+                              widget.markers[2*index+2].position.longitude,widget.markers[2*index+2].position.latitude);
+                      return Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
+                          color: Colors.lightBlue,),
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Spacer(),
+                            Text(
+                              '공유자 ${index + 1}',
+                              style: TextStyle(fontSize: 20.0, color: Colors.white),
                             ),
-                          ),
-                          Spacer(),
-                          Text(
-                            '출발거리 차이: ${distance1.toStringAsFixed(3)} m',
-                            style: TextStyle(fontSize: 20.0, color: Colors.white),
-                          ),
-                          Spacer(),
-                          Text(
-                            '도착거리 차이: ${distance2.toStringAsFixed(3)} m',
-                            style: TextStyle(fontSize: 20.0, color: Colors.white),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                    );
+                            Spacer(),
+                            Container(
+                              width: 400,
+                              height: 400,
+                              child: GoogleMap(
+                                initialCameraPosition: _initialPosition,
+                                mapType: MapType.normal,
+                                myLocationEnabled: true,
+                                myLocationButtonEnabled: true,
+                                onMapCreated: (controller) {
+                                  setState(() {
+                                    mapController = controller;
+                                  });
+                                },
+                                onTap: (cordinate) {
+                                  mapController.animateCamera(CameraUpdate.newLatLng(cordinate));
+                                },
+                                markers:{widget.markers[0],widget.markers[2*index+1],widget.markers[2*index+2]},
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              '출발거리 차이: ${distance1.toStringAsFixed(3)} m',
+                              style: TextStyle(fontSize: 20.0, color: Colors.white),
+                            ),
+                            Spacer(),
+                            Text(
+                              '도착거리 차이: ${distance2.toStringAsFixed(3)} m',
+                              style: TextStyle(fontSize: 20.0, color: Colors.white),
+                            ),
+                            Spacer(),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  controller: _controller,
+                  onForward: (index, info) {
+                    _index = index;
+                    print(info.direction);
+                    setState(() {});
+                  },
+                  onBack: (index, info) {
+                    _index = index;
+                    setState(() {});
+                  },
+                  onEnd: () {
+                    print('end');
                   },
                 ),
-                controller: _controller,
-                onForward: (index, info) {
-                  _index = index;
-                  print(info.direction);
-                  setState(() {});
-                },
-                onBack: (index, info) {
-                  _index = index;
-                  setState(() {});
-                },
-                onEnd: () {
-                  print('end');
-                },
               ),
               SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  OutlinedButton(
-                    onPressed: () {
-                      _controller.back();
-                    },
-                    child: Text('되돌리기'),
-                  ),
-                  OutlinedButton(
-                    onPressed: () {
-                      _controller.forward();
-                    },
-                    child: Text('다른 공유자'),
-                  ),
-                  OutlinedButton(
-                    onPressed: () {
-                      _controller.reset();
-                    },
-                    child: Text('리셋'),
-                  ),
-                  OutlinedButton(
-                    onPressed: () {
-                      buttonTap=true;
-                      setState(() {
-                      });
-                    },
-                    child: Text('선택'),
-                  )
-                ],
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Spacer(),
+                    SizedBox(
+                      width: 70,
+                      height: 70,
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.deepOrangeAccent.shade100,
+                        onPressed: () {
+                          _controller.back();
+                        },
+                        child: Icon(Icons.settings_backup_restore,size: 40,color: Colors.deepOrangeAccent,),
+                      ),
+                    ),
+                    Spacer(),
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.green.shade100,
+                        onPressed: () {
+                          buttonTap=true;
+                          setState(() {
+                          });
+                        },
+                        child: Icon(Icons.check,size: 70,color: Colors.green,),
+                      ),
+                    ),
+                    Spacer(),
+                    SizedBox(
+                      width: 70,
+                      height: 70,
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.red.shade100,
+                        onPressed: () {
+                          _controller.forward();
+                        },
+                        child: Icon(Icons.cancel_outlined,size: 40,color: Colors.red,),
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
               ),
+
             ],
-          ),
+
         ):Container(),
         ]
       ),
